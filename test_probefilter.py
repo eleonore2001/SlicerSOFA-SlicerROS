b8d9d6bfb835ea4ddaf6717c2fe2a625a4ba8e97
Eleonore Germond <elegermond@gmail.com>
2025-06-12 13:52:59 +0200

    This branch focuses on the creation of an updated fem cube based on the ROI

--- test_probefilter.py
+++ test_probefilter.py
@@ -21,7 +21,7 @@ importlib.reload(slicersofa.util)
 importlib.reload(slicersofa.meshes)
 import copy
 simulating = True
-
+import time 
 
 import Sofa
 import Sofa.Core
@@ -45,6 +45,7 @@ from cosserat.usefulFunctions import pluginList
 from cosserat.cosseratObject import Cosserat
 
 class DeformationManager:
+    """Gestionnaire optimisé pour les déformations d'image en temps réel."""
     
     def __init__(self):
         self.transform_node = None
@@ -58,13 +59,20 @@ class DeformationManager:
     
  
     def initialize_deformation_system(self, initial_deformation_array, initial_positions_array):
-        #Initialise the deformation system, is to be called just once 
+        """Initialise le système de déformation une seule fois."""
         
         roi_markup_node = slicer.util.getNode("R")
-        target_node = slicer.util.getNode("liver")
+        # target_node = slicer.util.getNode("liver")
+        target_node = slicer.util.getNode("LiverSegmentation000-label")
+        if target_node.IsA("vtkMRMLLabelMapVolumeNode"):
+            print("Application de la déformation à un LabelMapVolume")
+        elif target_node.IsA("vtkMRMLModelNode"):
+            print("Application de la déformation à un Model")
+
         
-        print("Initialising...")
+        print("Initialisation du système de déformation...")
         
+        # 1. Créer la structure de données source (réutilisable)
         self.points = vtk.vtkPoints()
         self.vectors = vtk.vtkFloatArray()
         self.vectors.SetNumberOfComponents(3)
@@ -72,20 +80,20 @@ class DeformationManager:
         
         num_points = initial_deformation_array.shape[0]
         
-        #Fill the vtk type data
+        # Initialiser avec les données initiales
         for i in range(num_points):
             x, y, z = initial_positions_array[i]
             self.points.InsertNextPoint(x, y, z)
             dx, dy, dz = initial_deformation_array[i]
             self.vectors.InsertNextTuple([dx, dy, dz])
         
-        #Create the PolyData
+        # Créer l'unstructured grid source (réutilisable)
         self.source_data = vtk.vtkPolyData()
         self.source_data.SetPoints(self.points)
         self.source_data.GetPointData().SetVectors(self.vectors)
         self.source_data.GetPointData().SetActiveVectors("Displacement")
 
-        # Create the probe grid according to the ROI dimension
+        # 2. Créer la grille de destination (fixe)
         roi_bounds = [0] * 6
         roi_markup_node.GetRASBounds(roi_bounds)
         
@@ -102,45 +110,50 @@ class DeformationManager:
             int(roi_size[2] / grid_spacing[2]) + 1
         ]
         
-
+        bounds = [0, 0, 0, 0, 0, 0]
+        self.source_data.GetBounds(bounds)
+        print(f"BORNES des points source: {bounds}")
+        print(f"BORNES du ROI: {roi_bounds}")
+        # Créer la grille de probe (fixe)
         probe_grid = vtk.vtkImageData()
         probe_grid.SetDimensions(grid_dims)
         probe_grid.SetOrigin(roi_bounds[0], roi_bounds[2], roi_bounds[4])
         probe_grid.SetSpacing(grid_spacing)
         probe_grid.AllocateScalars(vtk.VTK_DOUBLE, 1)
         
-        # Create the Point Interpolator 
+        # 3. Créer le ProbeFilter (réutilisable)
         self.interpolator = vtk.vtkPointInterpolator()
         self.interpolator.SetInputData(probe_grid)
         self.interpolator.SetSourceData(self.source_data)
         
-        #Point Interpolator parameters
+        # Utiliser un kernel gaussien avec un rayon adaptatif
         gaussian_kernel = vtk.vtkGaussianKernel()
         gaussian_kernel.SetSharpness(2.0)  # Ajustez la netteté
         gaussian_kernel.SetRadius(10.0)    # Rayon d'influence
         
         self.interpolator.SetKernel(gaussian_kernel)
-        self.interpolator.SetLocator(vtk.vtkStaticPointLocator())  
+        self.interpolator.SetLocator(vtk.vtkStaticPointLocator())  # Accélération
         self.interpolator.Update()
 
         print("Source bounds:", self.source_data.GetBounds())
         print("Probe grid bounds:",self.interpolator.GetInput().GetBounds()) 
         
-        # Work with the output of Point Interpolator
+        # 4. Récupérer les données interpolées
         probed_grid = self.interpolator.GetOutput()
         probe_vtk_array = probed_grid.GetPointData().GetArray("Displacement")
         if probe_vtk_array is None:
             print("Erreur: Pas de données de déplacement dans la grille interpolée")
             return None
         
+        # Convertir en array NumPy
         probe_array = vtk.util.numpy_support.vtk_to_numpy(probe_vtk_array)
         print(np.max(probe_array))
 
-        # IMPORTANT: VTK use (Z,Y,X) but we use  (X,Y,Z)
+        # IMPORTANT: VTK utilise l'ordre (Z,Y,X) alors que nous définissons (X,Y,Z)
         probe_array_shape = (grid_dims[2], grid_dims[1], grid_dims[0], 3)  # (Z,Y,X,3)
         probe_array = probe_array.reshape(probe_array_shape)
                 
-        # Create the transform node
+        # 4. Créer le nœud de transformation (unique)
         self.transform_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLGridTransformNode")
         self.transform_node.SetName("OptimizedDeformation")
         
@@ -151,6 +164,7 @@ class DeformationManager:
         displacement_grid.SetSpacing(grid_spacing)
         displacement_grid.AllocateScalars(vtk.VTK_DOUBLE, 3)
         
+        # Configurer la transformation
         grid_transform = vtk.vtkGridTransform()
         grid_transform.SetDisplacementGridData(displacement_grid)
         grid_transform.SetInterpolationModeToLinear()
@@ -159,8 +173,11 @@ class DeformationManager:
         # Récupérer l'array de transformation (référence persistante)
         self.grid_array = slicer.util.arrayFromGridTransform(self.transform_node)
         
+        # 5. Appliquer à l'objet cible
         target_node.SetAndObserveTransformNodeID(self.transform_node.GetID())
-        
+        transform_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLTransformNode")
+
+        target_node.HardenTransform()
         
         print(f"Système initialisé !")
         self.initialized = True
@@ -168,6 +185,7 @@ class DeformationManager:
         self.update_deformation(initial_deformation_array, initial_positions_array)
         
     def update_deformation(self, deformation_array, positions_array):
+        """Met à jour rapidement la déformation avec de nouvelles données."""
         
         if not self.initialized:
             print("Erreur: Système non initialisé. Appelez initialize_deformation_system() d'abord.")
@@ -199,7 +217,7 @@ class DeformationManager:
         # print(f"tuple random dans probed_grid: {probed_grid.GetPointData().GetVectors().GetTuple3(745)}")
         probe_vtk_array = probed_grid.GetPointData().GetArray("Displacement")
         print(f"Max déplacement dans grid_array: {np.max(probe_vtk_array)}")
-        print(f"Shape de grid_array: {np.shape(probe_vtk_array)}")
+        # print(f"Shape de grid_array: {np.shape(probe_vtk_array)}")
         
         if probe_vtk_array is not None:
             probe_array = vtk.util.numpy_support.vtk_to_numpy(probe_vtk_array)
@@ -213,7 +231,6 @@ class DeformationManager:
             
             slicer.util.arrayFromGridTransformModified(self.transform_node)
             slicer.app.processEvents()
-
 
 def addConstraintPoint(parentNode, beamPath):
     constraintPointsNode = parentNode.addChild('constraintPoints')
@@ -318,6 +335,91 @@ def createFemCubeWithParams(parentNode, geometry,minVol,maxVol):
 
     return FemNode
 
+#those functions enable the creation of a cube at the updated ROI position (makes Slicer crashes for the moment)
+def removeCubeAndDependencies(rootNode):
+    try:
+        FemNode = rootNode.getChild('FemNode')
+        gelNode = cubeNode.getChild('gelNode')
+        surfaceNode = gelNode.getChild("surfaceNode")
+    except:
+        print("[INFO] Aucun cubeNode à supprimer.")
+        return
+
+    # --- Supprimer les objets de distanceStatsNode ---
+    distanceStatsNode = slidingPoint.getChild("distanceStatsNode")
+    if distanceStatsNode:
+    # Supprimer les objets un par un après les avoir récupérés
+        computeDistanceComponent = distanceStatsNode.getObject("computeDistanceComponent")
+    if computeDistanceComponent:
+        distanceStatsNode.removeObject(computeDistanceComponent)
+
+    pointsMulti = distanceStatsNode.getObject("pointsMulti")
+    if pointsMulti:
+        distanceStatsNode.removeObject(pointsMulti)
+
+    distanceStats = distanceStatsNode.getObject("distanceStats")
+    if distanceStats:
+        distanceStatsNode.removeObject(distanceStats)
+        print("bien supp")
+
+
+  
+    slidingPoint.removeChild(distanceStatsNode)
+
+    # --- Supprimer le ContactListener ---
+    contactListener = rootNode.getObject("contactListener")
+    if contactListener:
+        rootNode.removeObject(contactListener)
+
+    # --- Supprimer constraintPointsNode et tous ses objets automatiquement ---
+    gelNode = cubeNode.getChild("gelNode")
+    constraintPointsNode = gelNode.getChild("constraintPoints")
+    if constraintPointsNode:
+        gelNode.removeChild(constraintPointsNode)
+        print("bien supp 2")
+
+    # --- Supprimer le contrôleur (Animation) ---
+    for obj in rootNode.objects:
+        if obj.__class__.__name__ == "Animation":  # ou remplace par nom explicite si disponible
+            rootNode.removeObject(obj)
+    print("bien supp 3")
+    print("bien supp 4")
+    gelNode.removeChild("surfaceNode")
+    print("bien supp 3")
+    # FemNode.removeChild("gelVolume")
+    # FemNode.removeChild("GelSurface")
+    FemNode.removeChild("gelNode")
+    print("bien supp 4")
+    rootNode.removeChild("FemNode")
+    print("tout bien supp")
+
+def mapGelCubetoROI():
+    roiNode = slicer.util.getNode("R")  
+   # Récupération du centre et de la taille
+    center = [0.0, 0.0, 0.0]
+    roiNode.GetCenter(center)
+
+    xyz = [0,0,0]
+    radius = [0,0,0]
+    roiNode.GetXYZ(xyz)
+    roiNode.GetRadiusXYZ(radius)
+
+
+    # Calcul des points min et max (en coordonnées RAS)
+    minPoint = [center[i] - radius[i] for i in range(3)]
+    maxPoint = [center[i] + radius[i] for i in range(3)]
+
+    last_radius= radius
+
+    minPoint2= " ".join(f"{v:.2f}" for v in minPoint)
+    maxPoint2= " ".join(f"{v:.2f}" for v in maxPoint)
+    removeCubeAndDependencies(rootNode)
+    newcubeNode = createFemCubeWithParams(rootNode,FemParams,minPoint2,maxPoint2)
+    addDependencies(newcubeNode)
+    print("job done ! cube has been replaced")
+
+
+#the  mapGelCubetoROI function is completed but there is still a problem afterwards
 def addDependencies(cubeNode):
     gelNode = cubeNode.getChild('gelNode')
     # constraintPointNode = addConstraintPoint(
@@ -333,6 +435,7 @@ def addDependencies(cubeNode):
 
     constraintPointsNode.addObject('BarycentricMapping', useRestPosition="false", listening="1")
    
+    print("the depencies have been added 2")
 
     # @info : This is the constraint point that will be used to compute the distance between the needle and the volume
     conttactL = rootNode.addObject('ContactListener', name="contactListener",
@@ -349,6 +452,7 @@ def addDependencies(cubeNode):
     inputVolumeMo = constraintPointsNode.constraintPointsMo.getLinkPath()
     inputNeedleMo = slidingPoint.slidingPointMO.getLinkPath()
     outputDistanceMo = distanceStatsNode.distanceStats.getLinkPath()
+    print("the depencies have been added 3")
 
     # ---------------------------------------------------
     # @info: Start controller node
@@ -359,9 +463,45 @@ def addDependencies(cubeNode):
         'CosseratNeedleSlidingConstraint', name="computeDistanceComponent")
     distanceStatsNode.addObject('DifferenceMultiMapping', name="pointsMulti", input1=inputVolumeMo, lastPointIsFixed=0,
                                 input2=inputNeedleMo, output=outputDistanceMo, direction="@../../FramesMO.position")
+    print("the depencies have been added 4")
+
+def addConstraintPoint(parentNode, beamPath):
+    constraintPointsNode = parentNode.addChild('constraintPoints')
+    constraintPointsNode.addObject("PointSetTopologyContainer", name="constraintPtsContainer", listening="1")
+    constraintPointsNode.addObject("PointSetTopologyModifier", name="constraintPtsModifier", listening="1")
+    constraintPointsNode.addObject("MechanicalObject", template="Vec3d", showObject=True, showIndices=True,
+                                   name="constraintPointsMo", position=[], showObjectScale=0, listening="1")
+
+    # print(f' ====> The beamTip tip is : {dir(beamPath)}')
+    constraintPointsNode.addObject('PointsManager', name="pointsManager", listening="1",
+                                   beamPath="/solverNode/needle/rigidBase/cosseratInSofaFrameNode/slidingPoint"
+                                            "/slidingPointMO")
+
+    constraintPointsNode.addObject('BarycentricMapping', useRestPosition="false", listening="1")
+    return constraintPointsNode
+
+
+def imageDeformation_optimized(deformation_array, positions_array):
+    """Version optimisée qui utilise le gestionnaire de déformation."""
+    global deformation_manager
+    
+    # Initialiser si nécessaire
+    if not hasattr(imageDeformation_optimized, 'deformation_manager'):
+        imageDeformation_optimized.deformation_manager = DeformationManager()
+        imageDeformation_optimized.deformation_manager.initialize_deformation_system(
+            deformation_array, positions_array)
+    else:
+        # Mise à jour rapide
+        imageDeformation_optimized.deformation_manager.update_deformation(
+            deformation_array, positions_array)
+
+
+
 rootNode = Sofa.Core.Node("root")
 
+
 params = NeedleParameters()
+
 
 nbFrames = GeometryParams.nbFrames
 needleGeometryConfig = {'init_pos': [0., 0., 0.], 'tot_length': GeometryParams.totalLength,
@@ -430,6 +570,8 @@ plugin_list = [
 for plugin in plugin_list:
     rootNode.addObject("RequiredPlugin", name=plugin)
 
+
+
 rootNode.addObject('VisualStyle', displayFlags='showVisualModels showBehaviorModels hideCollisionModels '
                                                 'hideBoundingCollisionModels hideForceFields '
                                                 'hideInteractionForceFields hideWireframe showMechanicalMappings')
@@ -442,6 +584,7 @@ rootNode.addObject('RuleBasedContactManager',
                     responseParams='mu=0.1', response='FrictionContactConstraint')
 rootNode.addObject('BruteForceBroadPhase')
 rootNode.addObject('BVHNarrowPhase')
+# rootNode.addObject('LocalMinDistance', alarmDistance=1.0, contactDistance=0.01)
 rootNode.addObject('LocalMinDistance', name="Proximity", alarmDistance=0.5,
                     contactDistance=ContactParams.contactDistance,
                     coneFactor=ContactParams.coneFactor, angleCone=0.1)
@@ -454,6 +597,10 @@ gravity = [0, 0, 0]
 rootNode.gravity.value = gravity
 rootNode.dt = dt
 rootNode.addObject('BackgroundSetting', color='0 0.168627 0.211765')
+# rootNode.addObject('OglSceneFrame', style="Arrows", alignment="TopRight")
+# ###############
+# New adds to use the sliding Actuator
+###############
 solverNode = rootNode.addChild('solverNode')
 solverNode.addObject('EulerImplicitSolver',
                         rayleighStiffness=PhysicsParams.rayleighStiffness)
@@ -473,6 +620,7 @@ slidingPoint = needle.addSlidingPoints()
 # Start the volume definition
 # -----------------
 roiNode = slicer.util.getNode("R")  
+   # Récupération du centre et de la taille
 center = [0.0, 0.0, 0.0]
 roiNode.GetCenter(center)
 
@@ -481,8 +629,13 @@ radius = [0,0,0]
 roiNode.GetXYZ(xyz)
 roiNode.GetRadiusXYZ(radius)
 
+
+# Calcul des points min et max (en coordonnées RAS)
 minPoint = [center[i] - radius[i] for i in range(3)]
 maxPoint = [center[i] + radius[i] for i in range(3)]
+
+last_radius= radius
+
 minPoint2= " ".join(f"{v:.2f}" for v in minPoint)
 maxPoint2= " ".join(f"{v:.2f}" for v in maxPoint)
 
@@ -532,8 +685,8 @@ if PYVISTA:
         modelNode.SetName(name)
         modelNode.CreateDefaultDisplayNodes()
         modelNode.SetAndObservePolyData(mesh)
-    instrumentModel = slicer.util.getNode('needle')
-    instrumentModel.GetDisplayNode().SetOpacity(0.0)
+    # instrumentModel = slicer.util.getNode('needle')
+    # instrumentModel.GetDisplayNode().SetOpacity(0.0)
 
     needle_positions_quat=rootNode.solverNode.needle.rigidBase.cosseratInSofaFrameNode.FramesMO.position.array()
     needle_positions_write = needle_positions_quat[:, :3]
@@ -542,25 +695,24 @@ if PYVISTA:
         needle_positions[i][1]+= needle_positions[i][0]*0.01
     
 
-def imageDeformation_optimized(deformation_array, positions_array):
-    """Version optimisée qui utilise le gestionnaire de déformation."""
-    global deformation_manager
-    
-    # Initialiser si nécessaire
-    if not hasattr(imageDeformation_optimized, 'deformation_manager'):
-        imageDeformation_optimized.deformation_manager = DeformationManager()
-        imageDeformation_optimized.deformation_manager.initialize_deformation_system(
-            deformation_array, positions_array)
-    else:
-        # Mise à jour rapide
-        imageDeformation_optimized.deformation_manager.update_deformation(
-            deformation_array, positions_array)
+#Initialisation du pipeline de deformation d'image
+point_position = rootNode.FemNode.gelNode.tetras.position.array().copy() 
+point_rest_position = rootNode.FemNode.gelNode.tetras.rest_position.array().copy()
+initial_deformation_array = point_position - point_rest_position
+
 
 
 def timeStep():
-    global First
     Sofa.Simulation.animate(rootNode, rootNode.dt.value)
-    # print("time step active")
+    radius = [0,0,0]
+    roiNode.GetRadiusXYZ(radius)
+    if radius != last_radius:
+        print("changement de ROI ")
+        time.sleep(0.5)
+        mapGelCubetoROI()
+
+
+    radius = last_radius
     with rootNode.solverNode.needle.rigidBase.RigidBaseMO.rest_position.writeable() as posA:
         trans_node = slicer.util.getNode('ros2:tf2lookup:wristTotip')
         transformation_matrix_SOFA= slicer.util.arrayFromTransformMatrix(trans_node, toWorld = True)
@@ -611,7 +763,8 @@ def timeStep():
     point_position = rootNode.FemNode.gelNode.tetras.position.array().copy() 
     point_rest_position = rootNode.FemNode.gelNode.tetras.rest_position.array().copy()
     deformation_field = point_position - point_rest_position
-    imageDeformation_optimized(deformation_field, point_position)
+    # imageDeformationv2(deformation_field, point_position)
+    # imageDeformation_optimized(deformation_field, point_position)
     if simulating:
         qt.QTimer.singleShot(100, timeStep)
     
